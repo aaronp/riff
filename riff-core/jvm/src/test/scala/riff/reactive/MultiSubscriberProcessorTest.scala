0d36296d8766ee328302cadc04a6b3eddf8b57e8
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2018-11-07 06:48:19 +0000

    reduced num expected in threading test

--- riff-core/jvm/src/test/scala/riff/reactive/MultiSubscriberProcessorTest.scala
+++ riff-core/jvm/src/test/scala/riff/reactive/MultiSubscriberProcessorTest.scala
@@ -11,8 +11,8 @@ class MultiSubscriberProcessorTest extends RiffThreadedSpec {
       // a bunch of stuff which attempts to build confidence (though not prove) a subscriber to our  'MultiSubscriberProcessor'
       // will invoke 'onNext' in a threadsafe manner
       object setup {
-        val numItemsToPush = 1000
-        val numThreads = 10
+        val numItemsToPush = 500
+        val numThreads = 6
         val queueSize = numItemsToPush * numThreads
         val multiSubscriber = MultiSubscriberProcessor[String](queueSize, true)
 
